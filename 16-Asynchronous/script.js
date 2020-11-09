f707da33bc19982b0928a55583eadef60ee2b095
anmarkosyan <anush.markos@gmail.com>
2020-11-09 14:36:43 +0400

    how to handle an error

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -3,6 +3,32 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  //countriesContainer.style.opacity = '1';
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //countriesContainer.style.opacity = '1';
+};
 //=============== First AJAX Call: XMLHttpRequest ==================
 // const getCountryData = function (country) {
 //   const request = new XMLHttpRequest();
@@ -37,27 +63,7 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('France');
 
 //======================== Welcome to Callback Hell ===========================
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-    </article>
-  `;
 
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = '1';
-};
 /*
 const getCountryAndNeighbor = function (country) {
   //AJAX call country 1
@@ -117,7 +123,7 @@ setTimeout(() => {
 //now
 //const request = fetch('https://restcountries.eu/rest/v2/name/armenia')
 
-// #1 solution
+// #1 solution: How to CONSUME a promises
 // const getCountryData = function (country) {
 //   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
 //     .then(function (response) {
@@ -145,7 +151,16 @@ const getCountryData = function (country) {
       return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
     })
     .then(response => response.json())
-    .then(data => renderCountry(data, 'neighbour'));
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥💥 ${err.message} : Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = '1';
+    });
 };
 
-getCountryData('armenia');
+btn.addEventListener('click', function () {
+  getCountryData('armenia');
+});
