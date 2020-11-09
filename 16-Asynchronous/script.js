2cd8f687d7629cdff6ad04668673733592b64e15
anmarkosyan <anush.markos@gmail.com>
2020-11-09 13:28:44 +0400

    Promises and the Fetch API:modern way of making AJAX call

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -44,9 +44,13 @@ const renderCountry = function (data, className = '') {
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
             <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
           </div>
     </article>
   `;
@@ -54,7 +58,7 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = '1';
 };
-
+/*
 const getCountryAndNeighbor = function (country) {
   //AJAX call country 1
   const request = new XMLHttpRequest();
@@ -100,3 +104,34 @@ setTimeout(() => {
     }, 1000);
   }, 1000);
 }, 1000);
+
+ */
+
+//====================== Promises and the Fetch API:modern way of making AJAX call ======================
+
+//before
+//const request = new XMLHttpRequest();
+// request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+// request.send()
+
+//now #1
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+//now #2
+const getCountryData = function (country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+
+getCountryData('armenia');
