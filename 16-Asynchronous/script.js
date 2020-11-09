15eddb9fcdb168ca7e6c39f09b29f5f0ffbaa1f0
anmarkosyan <anush.markos@gmail.com>
2020-11-09 10:55:27 +0400

    another example of callback hell using timers

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -86,3 +86,17 @@ const getCountryAndNeighbor = function (country) {
 };
 
 getCountryAndNeighbor('armenia');
+
+//another example of Callback Hell
+setTimeout(() => {
+  console.log('1 second passed');
+  setTimeout(() => {
+    console.log('2 seconds passed');
+    setTimeout(() => {
+      console.log('3 seconds passed');
+      setTimeout(() => {
+        console.log('4 seconds passed')
+      }, 1000)
+    }, 1000);
+  }, 1000);
+}, 1000);
