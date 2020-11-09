97659e259794b74b3b81b31668c71608c964b3f6
anmarkosyan <anush.markos@gmail.com>
2020-11-09 10:30:39 +0400

    Welcome to Callback Hell

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,20 +1,45 @@
 'use strict';
+
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 //=============== First AJAX Call: XMLHttpRequest ==================
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
+// const getCountryData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+//   request.send();
+//
+//   request.addEventListener('load', function () {
+//     //console.log(this.responseText);
+//     const [data] = JSON.parse(this.responseText);
+//     //console.log(data);
+//
+//     const html = `
+//     <article class="country">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+//           </div>
+//         </article>
+//     `;
+//
+//     countriesContainer.insertAdjacentHTML('beforeend', html);
+//     countriesContainer.style.opacity = '1';
+//   });
+// };
+//
+// getCountryData('Armenia');
+// getCountryData('USA');
+// getCountryData('France');
 
-  request.addEventListener('load', function () {
-    //console.log(this.responseText);
-    const [data] = JSON.parse(this.responseText);
-    //console.log(data);
-
-    const html = `
-    <article class="country">
+//======================== Welcome to Callback Hell ===========================
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
           <img class="country__img" src="${data.flag}" />
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -23,14 +48,41 @@ const getCountryData = function (country) {
             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
             <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
           </div>
-        </article>
-    `;
+    </article>
+  `;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = '1';
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
+
+const getCountryAndNeighbor = function (country) {
+  //AJAX call country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    //render country 1
+    renderCountry(data);
+
+    //get neighbor country 2
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+    //AJAX call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    request2.send();
+    //add another callback in first callback
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryData('Armenia');
-getCountryData('USA');
-getCountryData('France');
+getCountryAndNeighbor('armenia');
