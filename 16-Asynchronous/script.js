6e28598c1ae51f5abe55ce349b791d97dc751748
anmarkosyan <anush.markos@gmail.com>
2020-11-13 18:24:17 +0400

    in what oreder is executed the code

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,29 +1,29 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
-
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies[0].name
-            }</p>
-          </div>
-    </article>
-  `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = '1';
-};
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
+//
+// const renderCountry = function (data, className = '') {
+//   const html = `
+//     <article class="country ${className}">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//               +data.population / 1000000
+//             ).toFixed(1)}</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${
+//               data.currencies[0].name
+//             }</p>
+//           </div>
+//     </article>
+//   `;
+//
+//   countriesContainer.insertAdjacentHTML('beforeend', html);
+//   countriesContainer.style.opacity = '1';
+// };
 
 // const renderError = function (msg) {
 //   countriesContainer.insertAdjacentText('beforeend', msg);
@@ -241,32 +241,39 @@ TEST COORDINATES 2: -33.933, 18.474
 GOOD LUCK 😀
 */
 
-const whereAmI = (lat, lng) => {
-  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
-    .then(res => {
-      if (!res.ok) throw new Error(`Cannot recoding (${res.status})💥`);
+// const whereAmI = (lat, lng) => {
+//   fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Cannot recoding (${res.status})💥`);
+//
+//       return res.json();
+//     })
+//     .then(data => {
+//       console.log(data);
+//       console.log(`You are in ${data.city}, ${data.country}`);
+//
+//       return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+//     })
+//     .then(res => {
+//       if (!res.ok) throw new Error(`Country not found (${res.status})`);
+//
+//       return res.json();
+//     })
+//     .then(data => {
+//       renderCountry(data[0]);
+//     })
+//     .catch(err => {
+//       console.error(`${err.message}`);
+//     });
+// };
+//
+// whereAmI(52.508, 13.381); //You are in Berlin, Germany
+// whereAmI(19.037, 72.873); //You are in Mumbai, India
+// whereAmI(-33.933, 18.474); //You are in Cape Town, South Africa
 
-      return res.json();
-    })
-    .then(data => {
-      console.log(data);
-      console.log(`You are in ${data.city}, ${data.country}`);
 
-      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
-    })
-    .then(res => {
-      if (!res.ok) throw new Error(`Country not found (${res.status})`);
-
-      return res.json();
-    })
-    .then(data => {
-      renderCountry(data[0]);
-    })
-    .catch(err => {
-      console.error(`${err.message}`);
-    });
-};
-
-whereAmI(52.508, 13.381); //You are in Berlin, Germany
-whereAmI(19.037, 72.873); //You are in Mumbai, India
-whereAmI(-33.933, 18.474); //You are in Cape Town, South Africa
+//order of execution
+console.log('test start');//1
+setTimeout(() => console.log('0 sec  timer'), 0);//4
+Promise.resolve('Resolve promise 1').then(res => console.log(res));//3
+console.log('test end')//2
