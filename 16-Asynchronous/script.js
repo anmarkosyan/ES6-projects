cbe455232ed0c4c5f25513948b11bbdb138f0302
anmarkosyan <anush.markos@gmail.com>
2020-11-15 18:19:41 +0400

    how to promisify geolocation API, and based on geolocation on any device, display the flag and country

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,34 +1,34 @@
 'use strict';
 
-// const btn = document.querySelector('.btn-country');
-// const countriesContainer = document.querySelector('.countries');
-//
-// const renderCountry = function (data, className = '') {
-//   const html = `
-//     <article class="country ${className}">
-//           <img class="country__img" src="${data.flag}" />
-//           <div class="country__data">
-//             <h3 class="country__name">${data.name}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${(
-//               +data.population / 1000000
-//             ).toFixed(1)}</p>
-//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//             <p class="country__row"><span>💰</span>${
-//               data.currencies[0].name
-//             }</p>
-//           </div>
-//     </article>
-//   `;
-//
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = '1';
-// };
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
-// const renderError = function (msg) {
-//   countriesContainer.insertAdjacentText('beforeend', msg);
-//   //countriesContainer.style.opacity = '1';
-//};
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  //countriesContainer.style.opacity = '1';
+};
 
 //=============== First AJAX Call: XMLHttpRequest ==================
 // const getCountryData = function (country) {
@@ -283,7 +283,9 @@ GOOD LUCK 😀
 // console.log('test end'); //2
 
 //======================= Creating  our own promises ======================================
+/*
 const lotteryPromise = new Promise(function (resolve, reject) {
+
   console.log('Lottery drew is happening 🔮');
 
   setTimeout(function () {
@@ -340,3 +342,60 @@ wait(1)
 //and this is a static method of a promise constructor , which will resolve immediately
 Promise.resolve('abc').then(response => console.log(response));
 Promise.reject(new Error('efg')).catch(err => console.error(err));
+
+
+ */
+//========================== how to promisify geolocation API ==================
+//the way of callback API
+// navigator.geolocation.getCurrentPosition(
+//   position => console.log(position),
+//   err => console.error(err)
+// );//2 execution
+
+//console.log('First execution!'); //1 execution
+
+//promisify
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    //or simply
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+//getPosition().then(pos => console.log(pos));
+
+const whereAmI = () => {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Cannot recoding (${res.status})💥`);
+      return res.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.eu/rest/v2/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
+    .then(data => {
+      renderCountry(data[0]);
+    })
+
+    .catch(err => {
+      console.error(`${err.message}`);
+    });
+};
+
+btn.addEventListener('click', whereAmI);
