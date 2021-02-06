d0d13bd9b5f0b2727d43b6987804503dc207c0e2
anmarkosyan <anush.markos@gmail.com>
2021-02-06 14:28:06 +0400

    coding challenge

--- 03-Developer-skills/script.js
+++ 03-Developer-skills/script.js
@@ -7,7 +7,7 @@
 //   alert("Your browser is really old!");
 // }else{
 //   alert('yes, everything is new)))))')
-// }//to test if a built-in Promise object exists (it doesn’t in really old browsers):
+// }//to test if a built-in Promise object exists (it does not in really old browsers):
 
 //================ ✏️How to solve a problem =====================
 /*
@@ -55,22 +55,71 @@ keep in mind that sometimes there might be  sensor error.
 // //2) Breaking down into sub-problems
 // //- Marge 2 arrays
 //
-const calcTempAmplitudeNew = function (t1, t2) {
-  const temps = t1.concat(t2);
-  console.log(temps);
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+//
+//   let maxNum = temps[0];
+//   let minNum = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > maxNum) maxNum = curTemp;
+//     if (curTemp < minNum) minNum = curTemp;
+//   }
+//   console.log(maxNum, minNum);
+//   return maxNum - minNum;
+// };
+//
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 1, 'error'], [4, -4, 2, -1]);
+// console.log(amplitudeNew);
 
-  let maxNum = temps[0];
-  let minNum = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
+//============== ✏️ Debugging with console =============
+/*
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
 
-    if (typeof curTemp !== 'number') continue;
-    if (curTemp > maxNum) maxNum = curTemp;
-    if (curTemp < minNum) minNum = curTemp;
-  }
-  console.log(maxNum, minNum);
-  return maxNum - minNum;
+    //Fix bug
+    value: Number(prompt('Degrees celsius!')),
+
+  };
+
+  //Find bug
+  //console.table(measurement);
+  //console.log(measurement.value);∂
+  //console.warn(measurement.value);
+  //console.error(measurement.value);
+
+  return measurement.value + 273;
 };
 
-const amplitudeNew = calcTempAmplitudeNew([3, 5, 1, 'error'], [4, -4, 2, -1]);
-console.log(amplitudeNew);
+console.log(measureKelvin()); //10 + 273 = 283
+ */
+
+//================= 👩🏻‍💻 coding challenge =================
+/*
+ Given an array of maximum temperatures and should return a string with these temperatures.
+ Create a function which takes an array, and return a string
+
+*/
+
+const maxTemps = [17, 21, 23];
+
+function printForecast(arr) {
+  //create an empty string
+  let displayString = '';
+  //loop over the array
+  for (let i = 0; i < arr.length; i++) {
+    displayString += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+
+  //return string
+  return '... ' + displayString;
+}
+console.log(globalThis);
+
+console.log(printForecast(maxTemps)); //'... 17°C in 1 days ... 21°C in 2 days ... 23°C in 3 days ...'
+console.log(printForecast([12, 5, -5, 0, 4])); //'... 12°C in 1 days ... 5°C in 2 days ... -5°C in 3 days ... 0°C in 4 days ... 4°C in 5 days ...
