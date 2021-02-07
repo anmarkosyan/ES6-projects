e456e8c146834195a5a19c160290595c73f6a9dd
anmarkosyan <anush.markos@gmail.com>
2021-02-07 15:01:46 +0400

    implemented game logic

--- 04-Guess-My-Number-Project/script.js
+++ 04-Guess-My-Number-Project/script.js
@@ -1,3 +1,26 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct number!';
+console.log(document.querySelector('.message').textContent);
+
+const num = Number(document.querySelector('.number').textContent = '13');
+console.log(num);
+
+const score = Number(document.querySelector('.score').textContent = '13');
+console.log(score)
+
+const input = (document.querySelector('.guess').value = 23);
+console.log(input);
+
+ */
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  }
+});
