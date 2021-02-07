2618ae35a1162749deaffc8fe2a133775faeec71
anmarkosyan <anush.markos@gmail.com>
2021-02-07 19:09:25 +0400

    finish game

--- 04-Guess-My-Number-Project/script.js
+++ 04-Guess-My-Number-Project/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,6 +20,12 @@ document.querySelector('.check').addEventListener('click', function () {
     //using CSS
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    //set higher scope
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     //📌 when guess number is too high
   } else if (guess > secretNumber) {
