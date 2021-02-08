951e61fb93e1a5e035a75a9d3f2ee128bea0e604
anmarkosyan <anush.markos@gmail.com>
2021-02-08 13:38:56 +0400

    working with Escape keyboard event

--- 05-Modal-Project/script.js
+++ 05-Modal-Project/script.js
@@ -27,3 +27,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModel);
 overlay.addEventListener('click', closeModel);
+
+document.addEventListener('keydown', function (e) {
+  //console.log(e);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModel();
+  }
+});
