aa21467804eada4fe96f2d606e2fcc605000bcf2
anmarkosyan <anush.markos@gmail.com>
2021-02-08 16:30:46 +0400

    game logic: 1

--- 06-Pig-Game-Project/script.js
+++ 06-Pig-Game-Project/script.js
@@ -1,7 +1,42 @@
 'use strict';
 
 //1️⃣ Selectors
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const score0 = document.getElementById('current--0');
+const score1 = document.getElementById('current--1');
 
-//2️⃣ Functions
+const displayDice = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+
+//2️⃣ Variables and Functions
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+displayDice.classList.add('hidden');
+let currentScore0 = 0;
+let currentScore1 = 0;
 
 //3️⃣ Event Listeners
+btnRoll.addEventListener('click', function () {
+  //1.generation a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
+
+  //2.display dice roll
+  displayDice.classList.remove('hidden');
+  displayDice.src = `img/dice-${dice}.png`;
+
+  //3. check if number for 1:
+  //if not: add number to the current score
+  if (dice !== 1) {
+    currentScore0 += dice;
+    score0.textContent = currentScore0;
+
+    // if true: switch to the next player
+  } else {
+    score0El.textContent = currentScore0;
+  }
+});
