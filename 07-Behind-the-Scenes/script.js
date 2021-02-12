382b1d8dbef69d8cce55e6ac3993563faf81440a
anmarkosyan <anush.markos@gmail.com>
2021-02-12 13:09:12 +0400

    how work with hoist variables and functions

--- 07-Behind-the-Scenes/script.js
+++ 07-Behind-the-Scenes/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//1️⃣ SCOPING
+/*
 function calcAge(birthYear) {
   const age = 2021 - birthYear;
   //console.log(firstName);
@@ -28,3 +30,57 @@ function calcAge(birthYear) {
 //calcAge(1986);// reference error: we cannot call the function before declaration firstName variable;
 const firstName = 'Anush';
 calcAge(1986); //Anush
+
+
+ */
+
+//2️⃣ HOISTING
+
+//❗️hoisting variables:
+//using before declaring them
+//console.log(me); //undefined
+//console.log(job); //Uncaught ReferenceError: Cannot access 'job' before initialization
+//console.log(year); //Uncaught ReferenceError: Cannot access 'year' before initialization
+
+var me = 'Anush';
+let job = 'developer';
+const year = 1986;
+
+// console.log(me === window.me);//true => will create a property on the global window object
+// console.log(job === window.job);//false
+// console.log(year === window.year);//false
+
+
+//❗️hoisting functions
+//using before declaring them
+//console.log(addDecl(3, 5)); // 8
+//console.log(addExp(3, 5));//Uncaught ReferenceError: Cannot access 'addExp' before initialization
+//console.log(addArw(3, 5));//Uncaught ReferenceError: Cannot access 'addArw' before initialization
+//console.log(addArw1(2, 3));//addArw1 is not a function => because it is undefined
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExp = function (a, b) {
+  return a + b;
+};
+
+const addArw = (a, b) => a + b;
+
+var addArw1 = (a, b) => a + b;
+
+//console.log(addDecl(2,3) === window.addDecl(2,3));//true
+//console.log(addExp(2,3) === window.addExp(2,3));//window.addExp is not a function
+//console.log(addArw(2,3) === window.addArw(2,3));//window.addArw is not a function
+
+// ‼️ examples that shouldn't happen
+//console.log(numProducts); //undefined
+if (!numProducts) deleteShoppingCard(); //'All products deleted!!!'
+//if(true) deleteShoppingCard();
+
+var numProducts = 10;
+
+function deleteShoppingCard() {
+  console.log('All products deleted!!!');
+}
