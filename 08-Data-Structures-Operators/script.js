9be624e725725288f30b33b52da5a9b9a4b89482
anmarkosyan <anush.markos@gmail.com>
2021-02-16 12:09:26 +0400

    work with spread operator

--- 08-Data-Structures-Operators/script.js
+++ 08-Data-Structures-Operators/script.js
@@ -1,5 +1,43 @@
 'use strict';
 
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIdx, mainIdx) {
+    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
+  },
+
+  orderDelivery: function ({ time, address, mainIndex, starterIndex }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
+       will be delivered ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+};
+
 //1️⃣ Destructuring arrays
 /*
 const arr = [2, 3, 4];
@@ -13,18 +51,6 @@ console.log(a, b, c); // 2, 3, 4
 const [x, y, z] = arr;
 console.log(x, y, z); //2, 3, 4
 console.log(arr); //[2, 3, 4] don't change main arr
-
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
-  order: function (starterIdx, mainIdx) {
-    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
-  },
-};
 
 //📍 => basic example
 const [first, second] = restaurant.categories;
@@ -64,40 +90,8 @@ console.log(e, t, u);//8 9 1
  */
 
 //2️⃣ Destructuring objects
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-
-  order: function (starterIdx, mainIdx) {
-    return [this.starterMenu[starterIdx], this.mainMenu[mainIdx]];
-  },
-
-  orderDelivery: function ({ time, address, mainIndex, starterIndex }) {
-    console.log(
-      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} 
-       will be delivered ${address} at ${time}`
-    );
-  },
-};
-
+/*
 //📍order food
 restaurant.orderDelivery({
   time: '22:30',
@@ -134,3 +128,52 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c); //11 23
+
+ */
+//3️⃣ The Spread operator => whenever we need the elements
+// of an array individually,then we can use the spread operator
+//‼️ Only work when we pass spread argument into a function and when build a new array ‼️
+
+//📍add in other array
+const arr = [7, 3, 4];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+const goodNewArr = [12, 23, ...arr];
+console.log(badNewArr); //[1, 2, 7, 3, 4]
+console.log(goodNewArr); // [12, 23, 7, 3, 4]
+console.log(...goodNewArr); //12 23 7 3 4
+
+//📍 created newMenu , and add new item
+const newMenu = [...restaurant.mainMenu, 'Avocado'];
+console.log(newMenu);
+
+//📍copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+//📍Join 2 arrays together
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+//📍Iterables: strings, arrays, maps, sets but NOT objects
+const str = 'Mark';
+const letter = [...str];
+console.log(letter); //["M", "a", "r", "k"]
+console.log(...letter); //M a r k
+//console.log(`${...letter} letter`);//Uncaught SyntaxError: Unexpected token '...'
+
+//📍using into functions
+// const ingredients = [
+//   prompt("Let's make a pasta! Ingredients 1?"),
+//   prompt('Ingredients 2?'),
+//   prompt('Ingredients 3?'),
+// ];
+// console.log(ingredients);//["a", "b", "c"]
+// restaurant.orderPasta(...ingredients);
+
+//📍 since ES 2018, spread operator also work on objects
+//shallow copy of main object: {...restaurant} === Object.assign({}, restaurant);
+const newRestaurant = { foundedIn: 2000, ...restaurant, founder: 'Aram' };
+console.log(newRestaurant);
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Aram's house";
+console.log(restaurantCopy.name);//Aram's house
+console.log(restaurant.name);//Classico Italiano
