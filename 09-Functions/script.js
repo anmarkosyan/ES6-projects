73ce5a9e476a53cbc3bd1d2cce599a75877d09cc
anmarkosyan <anush.markos@gmail.com>
2021-02-23 16:44:19 +0400

    functions: default parameters

--- 09-Functions/script.js
+++ 09-Functions/script.js
@@ -1,2 +1,29 @@
 'use strict';
 
+//1️⃣ Default parameters:
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  //old way of setting default parameters:ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+};
+
+createBooking('AB231');
+//override default parameters
+createBooking('AB231', 2, 300);
+createBooking('AB231', 2);
+createBooking('AB231', 4);
+
+//if we want to skip the one argument and specify another, for doing that we should write UNDEFINED, which will take the default value
+createBooking('AB231', undefined, 1000); //{flightNum: "AB231", numPassengers: 1, price: 1000}
