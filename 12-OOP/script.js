e9a15d500e7c23c729c11f638285806471b99545
anmarkosyan <anush.markos@gmail.com>
2021-06-09 11:56:03 +0400

    about inheritance

--- 12-OOP/script.js
+++ 12-OOP/script.js
@@ -1,10 +1,31 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
-  console.log(this);//Person {}
   this.firstName = firstName;
   this.birthYear = birthYear;
 };
-//call function
+
 const anush = new Person('Anush', 1986);
-console.log(anush);//Person { firstName: 'Anush', birthYear: 1986 }
+const aram = new Person('Aram', 1985);
+
+// const user1Prototype = Reflect.getPrototypeOf(anush);
+// const user2Prototype = Reflect.getPrototypeOf(aram);
+//
+// console.log(anush === aram);
+// console.log(user1Prototype === user2Prototype);
+
+console.log(anush instanceof Person);//true
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function(){
+  console.log(2021 - this.birthYear);
+}
+anush.calcAge();
+console.log(anush.__proto__);
+console.log(Person.prototype.isPrototypeOf(anush));//true
+console.log(Person.prototype.isPrototypeOf(Person));//false
+
+
+
+
+
